acf12f4e064d8a96f7a888071d04e924dc4ce397
zacgumm <zacgumm@mail.mvnu.edu>
2017-09-21 13:53:21 -0400

    Pre Challenge Six

--- app/src/main/java/zacha/gumm/geoquiz/CheatActivity.java
+++ app/src/main/java/zacha/gumm/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package zacha.gumm.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -55,8 +59,27 @@ public class CheatActivity extends AppCompatActivity {
                 setAnswerShownResult(true);
                 Question currentQuestion = QuizActivity.getQuestion();
                 currentQuestion.setQuestionCompromised(true);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
-        });
+            }
+        );
     }
 
 }
